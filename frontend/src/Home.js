80e59f81abc3e01531fba57e41fef818d25a3238
gainduamarasinghe <gaindu2k03@gmail.com>
2024-09-06 19:10:43 +0530

    first commit

---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <h2 className='welcome'>Welcome!</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/SignupValidation.js
@@ -0,0 +1,31 @@
+function Validation(values) {
+  let error = {};
+  const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const password_pattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (values.name === "") {
+    error.name = "Name should not be empty";
+  }else{
+    error.name = "";
+  }
+
+  if (values.email === "") {
+    error.email = "Email should not be empty";
+  } else if (!email_pattern.test(values.email)) {
+    error.email = "Email Didn't match";
+  } else {
+    error.email = "";
+  }
+
+  if (values.password_pattern === "") {
+    error.password = "password should not be empty";
+  } else if (!password_pattern.test(values.password)) {
+    error.password = "password didn't match";
+  } else {
+    error.password = "";
+  }
+
+  return error;
+}
+export default Validation;
